8c68cba7db436c23c46e27be60ad0279d823d6ba
chaim1026 <chaim1026@gmail.com>
2020-06-21 15:33:53 +0300

    xp

---
+++ week 2/day 1/xp/excersize 3.js
@@ -0,0 +1,5 @@
+let string = "I am trying to find nemo in this string"
+let array = string.split(" ")
+console.log(array)
+let position = array.indexOf("nemo")
+console.log(`I found nemo at ${position}`)
