f195d0934c21a992d16b3b26b656a7a1ad98b1c6
chaim1026 <chaim1026@gmail.com>
2020-06-21 20:13:17 +0300

    add

---
+++ week 2/day 1/xp gold/xp gold.js
@@ -0,0 +1,8 @@
+// let me = ["my", "favorite", "color", "is", "blue"]
+// let string = me.join()
+// console.log(string)
+
+
+let string1 = "hello"
+let string2 = "world"
+console.log(string1.substr(0, 1) + string2.substr(1, string2.length) + " " + string2.substr(0, 1) + string1.substr(1, string1.length));
