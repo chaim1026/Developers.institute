4adfd100a6057035c3cd532e4f9aee1f94e2a1b3
chaim1026 <chaim1026@gmail.com>
2020-06-24 00:08:07 +0300

    day 2 dc

--- week 2/day 2/daily challenge/day 2 dc.js
+++ week 2/day 2/daily challenge/day 2 dc.js
@@ -1,11 +1,12 @@
-let string = 'the train ride to school was not that bad today'
+let string = prompt("please enter a sentence with both not and bad")
 let array = string.split(" ")
 let position1 = array.indexOf("not")
 let position2 = array.indexOf("bad")
 console.log(position1)
 console.log(position2)
-if (position1 < position2) {
-    console.log('the train ride to school was good')
-} else {
-    console.log(string)
+if (position1 < position2 && position1 > -1) {
+    array.splice(position1, position2 - position1 + 3, "good")
+    string = array.join(" ")
+    console.log(" ")
 }
+console.log(string)
