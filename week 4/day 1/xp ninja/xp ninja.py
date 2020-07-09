53ff5c30d5201dc97d4b5e21d752fb2697815bbb
Chaim <chaim1026@gmail.com>
2020-07-09 22:11:46 +0300

    ninja day 1

---
+++ week 4/day 1/xp ninja/xp ninja.py
@@ -0,0 +1,40 @@
+"""def hello_print(a,b):
+    if a > b:
+        print("hello world")
+hello_print(5,3)    """
+
+
+"""month = int(input("Please enter a month from 1 to 12\n"))
+if month >= 3 and month <= 5:
+    print("Spring time")
+elif month >= 6 and month <= 8:
+    print("Summer time")
+elif month >= 9 and month <= 11:
+    print("Autumn time")
+else:
+    print("Winter time")
+"""
+
+
+"""my_text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+my_text = my_text.split(" ")
+my_text = "".join(my_text)
+length_of_string = len(my_text)
+print(length_of_string)"""
+
+
+"""users_string = input("I challenge you to write the longest sentance without the letter a:\n")
+my_string = "heyhowyoudoingitsbeenforeversinceweweretogether"
+length_my_string = len(my_string)
+users_string = users_string.split(" ")
+users_string = "".join(users_string)
+length_user_string = len(users_string)
+for letter in users_string:
+    if letter == "a" or letter == "A":
+        raise TypeError("you cheated looser")
+if length_user_string > length_my_string:
+    print(length_user_string)
+else:
+    print("you lose looser")"""
+
+
