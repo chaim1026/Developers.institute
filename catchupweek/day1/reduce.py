a9b2ad13b3fc7f7fa135169eb2f068f95acd0a4a
Chaim <chaim1026@gmail.com>
2020-07-23 14:50:57 +0300

    adding updates

--- catchupweek/day1/reduce.py
+++ catchupweek/day1/reduce.py
@@ -1,13 +1,20 @@
 # syntax functools.reduce(function, iterable[, initializer])
 
-from functools import reduce
+# from functools import reduce
 
-def num_add(a,b):
-    return a + b
+# def num_add(a,b):
+#     return a + b
 
-numbers = [1,2,3,4,5,6,7,8,9]
+# numbers = [1,2,3,4,5,6,7,8,9]
 
-list_value = reduce(num_add,numbers,100)
+# list_value = reduce(num_add,numbers,100)
 
-print(list_value)
+# print(list_value)
 
+
+
+def add_line(file_path,string,line_num):
+    with open(file_path,"a+") as f:
+        for i in range(line_num):
+            line = f.readline()
+        f.write(string)
--- week 5/day 5/mini prodject 2/anagrams.py
+++ week 5/day 5/mini prodject 2/anagrams.py
@@ -2,23 +2,22 @@ from anagram_checker import AnagramChecker
 
 class AnagramProgram(AnagramChecker):
     def menu(self):
-        self.choice = (input("please enter a word or exit to leave this program:\n").lower())
+        self.choice = (input("please enter a word or (exit) to leave this program:\n").lower())
         return self.choice
 
     def main(self):
         self.choice = self.menu()
         while self.choice != "exit":
-            self.choice = self.choice.lstrip()
-            self.choice = self.choice.rstrip()
+            self.choice = self.choice.lstrip() and self.choice.rstrip()
             if len(self.choice.split()) > 1 or not self.choice.isalpha():
                 raise ValueError("you entered more than one word")
             self.valid = self.is_valid_word(self.choice)
             if self.valid:
                 self.anagram_list = self.get_anagrams(self.choice)
-                print(f"YOU WORD: {self.choice}\nthis is a valid english word\nAnagrams for your word: {self.anagram_list}")
+                print(self)
             else:
                 return "not a valid word"
             self.choice = self.menu()
-            
-    # def __repr__(self):
-    #     return f"YOU WORD: {self.choice}\nthis is a valid english word\nAnagrams for your word: {self.anagram_list}"
+
+    def __repr__(self):
+        return f"YOU WORD: {self.choice}\nthis is a valid english word\nAnagrams for your word: {self.anagram_list}\n"
